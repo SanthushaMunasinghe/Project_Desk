d21ebd27431a7e5c72a4e1d66535cc021e6a1add
Santhusha Munasinghe <santhushamunasinghe601@gmail.com>
2023-01-15 21:16:14 +0530

    add routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { CreateProjectFormComponent } from './create-project-form/create-project-form.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
+import { MessagePageComponent } from './message-page/message-page.component';
+
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'dashboard', component: UserDashboardComponent },
+  { path: 'create', component: CreateProjectFormComponent },
+  { path: 'project', component: ProjectPageComponent },
+  { path: 'message', component: MessagePageComponent },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginPageComponent,
+    UserDashboardComponent,
+    CreateProjectFormComponent,
+    ProjectPageComponent,
+    MessagePageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
