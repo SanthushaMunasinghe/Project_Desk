a295ca4f868afdbf6bb59220eda5333a65b52016
Santhusha Munasinghe <santhushamunasinghe601@gmail.com>
2023-01-18 19:32:50 +0530

    frontend changes

--- frontend/src/app/components/project-card/project-card.component.ts
+++ frontend/src/app/components/project-card/project-card.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { faPeopleArrows } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.css']
+  styleUrls: ['./project-card.component.css'],
 })
 export class ProjectCardComponent {
-
+  teamworkIcon = faPeopleArrows;
+  projectTitle = 'Project Title';
 }
